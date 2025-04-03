ca1fa9b7cc66da3efa7c23e6893b65435e8ff802
PRITAM MONDAL <131168290+RESK3N@users.noreply.github.com>
2025-04-04 00:50:21 +0530

    update

--- Merge.c
+++ Merge.c
@@ -1,4 +1,66 @@
 #include <stdio.h>
+void merge(int arr[],int left,int mid,int right){
+    int i,j,k;
+    int l1,l2;
+    l1=mid-left+1;
+    l2=right-mid;
+    int larr[l1],rarr[l2];
+    for(i=0;i<l1;i++){
+        larr[i]=arr[left+i];
+    }
+    for(j=0;j<l2;j++){
+        rarr[j]=arr[mid+1+j];
+    }
+    i=0,j=0,k=left;
+    while(i<l1 && j<l2){
+        if(larr[i]<=rarr[j]){
+            arr[k]=larr[i];
+            i++;
+        }else{
+            arr[k]=rarr[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<l1){
+        arr[k]=larr[i];
+        i++;
+        k++;
+    }
+    while(j<l2){
+        arr[k]=rarr[j];
+        j++;
+        k++;
+    }
+
+}
+void mergesort(int arr[],int left , int right){
+
+    if(left<right){
+        int mid=left+(right-left)/2;
+    mergesort(arr,left,mid);
+    mergesort(arr,mid+1,right);
+    merge(arr,left,mid,right);
+    }
+
+}
+void print(int arr[],int n){
+    for (int i = 0; i < n; ++i) {
+        printf("%d\t\t",arr[i]);
+
+    }
+}
+
 int main(){
-    printf("Hello");
+    int n=6;
+    printf("Enter Length of array : ");
+    scanf("%d",&n);
+    int arr[n] ;
+    for (int i = 0; i < n; ++i) {
+        printf("\nEnter %d element : ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    mergesort(arr,0,n-1);
+    print(arr,n);
+
 }
