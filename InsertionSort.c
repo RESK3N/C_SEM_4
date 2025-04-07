cfddfecb5769292d87a54ac9876ccc865f3b7600
PRITAM MONDAL <131168290+RESK3N@users.noreply.github.com>
2025-04-07 19:48:29 +0530

    update

---
+++ InsertionSort.c
@@ -0,0 +1,25 @@
+//Insertion Sort
+#include<stdio.h>
+void sort(int arr[], int n){
+    int key,j;
+    for(int i=1;i<n;i++){
+        key=arr[i];
+        j=i-1;
+        while(j>=0&&arr[j]>key){
+        arr[j+1]=arr[j];
+        j--;
+        }
+        arr[j+1]=key;
+    }
+}
+int main(){
+    int arr[]={5,4,10,1,2,6};
+    int len=sizeof(arr)/sizeof(arr[0]);
+    
+    for(int i=0;i<len;i++)
+        printf("%d\t",arr[i]);
+    sort(arr,len);
+    printf("\nAfter Sorting :\n");
+    for(int i=0;i<len;i++)
+    printf("%d\t",arr[i]);
+}
